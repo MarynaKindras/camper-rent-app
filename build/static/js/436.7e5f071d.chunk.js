bdc023d2d006a60c56f2eeb385e5953fc6d686b5
MarynaKindras <maryna.kindras@gmail.com>
2024-04-21 19:20:35 +0300

    Revert "Build"
    This reverts commit ef9285da582d8c3fdc9cfef79bccee9d991f8457.

--- build/static/js/436.7e5f071d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcamper_rent_app=self.webpackChunkcamper_rent_app||[]).push([[436],{436:(e,p,a)=>{a.r(p),a.d(p,{default:()=>s});var r=a(579);const s=()=>(0,r.jsx)("h1",{})}}]);
-//# sourceMappingURL=436.7e5f071d.chunk.js.map
