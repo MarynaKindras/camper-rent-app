dda80a674e0f25b5799ec552841d6ff3400ee591
MarynaKindras <maryna.kindras@gmail.com>
2024-04-26 21:39:07 +0300

    Update Home Page and Favorites

--- src/pages/HomePage/HomePage.module.js
+++ src/pages/HomePage/HomePage.module.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const InfoWrapper = styled.div`
   display: flex;
   padding: 100px 64px;
   flex-direction: column;
   align-items: center;
+  text-align: center;
 `;
 
 export const InfoImages = styled.div`
@@ -17,7 +19,14 @@ export const InfoImg = styled.img`
   margin-bottom: 60px;
 `;
 
-export const InfoTitle = styled.h1`
+export const ListItem = styled.h1`
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    color: #e44848;
+  }
+`;
+
+export const ItemsLink = styled(NavLink)`
   font-size: 60px;
   margin-bottom: 30px;
 `;
